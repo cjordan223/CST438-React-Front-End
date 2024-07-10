124593a8757e1b36df8780460898ba7196dd9e31
Erin <126372717+esmaj234@users.noreply.github.com>
2024-07-09 23:08:06 -0700

    Revert "V1.0"

--- src/components/instructor/AssignmentUpdate.js
+++ src/components/instructor/AssignmentUpdate.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Dialog,DialogActions, DialogContent, DialogTitle, Button, TextField } from '@mui/material';
 
 //  instructor updates assignment title, dueDate 
 //  use an mui Dialog
@@ -7,49 +6,10 @@ import { Dialog,DialogActions, DialogContent, DialogTitle, Button, TextField } f
 
 const AssignmentUpdate = (props)  => {
 
-  const[isOpen, setOpen] = useState(false);
-  const [message, setMessage] = useState('');
-  const [assignment, setAssignment] = useState({id:'', title:'', dueDate:'', secId:''})
-
-  const editOpen = () => {
-    setOpen(true);
-    setMessage('');
-    setAssignment(props.assignment);
-  }
-
-  const editClose = () => {
-    setOpen(false);
-  }
-
-  const editChange = (event) => {
-    setAssignment({ ...assignment, [event.target.name]: event.target.value})
-  }
-
-  const onSave = () => {
-    if (assignment.title==='' || assignment.dueDate==='' || assignment.secId==='') {
-      setMessage("Please fill in all fields");
-    } else {
-      props.save(assignment);
-      editClose();
-    }
-  }
 
     return (
         <>
-          <Button onClick={editOpen}>Edit</Button>
-          <Dialog open={isOpen}>
-            <DialogTitle> Edit Assignment </DialogTitle>
-            <DialogContent style={{paddingTop: 20}}>
-              <h4>{message}</h4>
-              <TextField style={{padding: 10}} fullWidth label="id" name="id" value={assignment.id} InputProps={{readOnly: true, }} />
-              <TextField style={{padding: 10}} autoFocus fullWidth label="title" name='title' value={assignment.title} onChange={editChange} />
-              <TextField style={{padding: 10}} fullWidth label="dueDate" name="dueDate" value={assignment.dueDate} onChange={editChange} />
-            </DialogContent>
-            <DialogActions>
-              <Button color='secondary' onClick={editClose}>Close</Button>
-              <Button color='primary' onClick={onSave}>Save</Button>
-            </DialogActions>
-          </Dialog>
+          <h3>Not implemented</h3>  
         </>                       
     )
 }
--- src/components/instructor/AssignmentsView.js
+++ src/components/instructor/AssignmentsView.js
@@ -1,12 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {useLocation} from 'react-router-dom';
-import { Button } from '@mui/material';
-import 'react-confirm-alert/src/react-confirm-alert.css';
-import { confirmAlert } from 'react-confirm-alert';
-import AssignmentUpdate from './AssignmentUpdate';
-import AssignmentGrade from './AssignmentGrade';
-import AssignmentAdd from './AssignmentAdd';
-import { SERVER_URL } from '../../Constants';
+import {useLocation} from 'react-router-dom'
 
 // instructor views assignments for their section
 // use location to get the section value 
@@ -18,145 +11,12 @@ import { SERVER_URL } from '../../Constants';
 
 const AssignmentsView = (props) => {
 
-    const [assignments, setAssignments] = useState([]);
-    const [message, setMessage] = useState('');
-    
     const location = useLocation();
-    const {s} = location.state;
-
-    const fetchAssignments = async () => {
-        try {
-            const response = await fetch(`${SERVER_URL}/sections/${s.secNo}/assignments`);
-            if (response.ok) {
-                const assignments = await response.json();
-                setAssignments(assignments);
-            } else {
-                const json = await response.json();
-                setMessage('Response error: ' +json.message);
-            }
-        } catch (err) {
-            setMessage('Network error '+err);
-        }
-    }
-
-    useEffect( () => {
-        fetchAssignments();
-    }, []);
-
-    const headers = ['Assignment ID', 'Title', 'Due Date', 'Section No', '', ''];
-
-    const deleteAlert = (event) => {
-        confirmAlert({
-            title: 'Confirm delete',
-            message: 'Do you really want to delete?',
-            buttons: [
-                {
-                    label: 'Yes',
-                    onClick: () => doDelete(event)
-                },
-                {
-                    label: 'No'
-                }
-            ]
-        });
-    }
-
-    const doDelete = (event) => {
-        const row_index = event.target.parentNode.parentNode.rowIndex -1;
-        deleteAssignment(assignments[row_index].id);
-    }
-
-    const deleteAssignment = async (id) => {
-        try {
-            const response = await fetch (`${SERVER_URL}/assignments/${id}`,
-                {
-                    method: 'DELETE',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                });
-            if (response.ok) {
-                setMessage('Assignment deleted');
-                fetchAssignments();
-            } else {
-                const rc = await response.json();
-                setMessage("Delete failed " +rc.message);
-            } 
-        } catch (err) {
-            setMessage("Network error: " + err);
-        }
-    }
-
-    
-    const onSave = async (assignment) => {
-        try {
-            const response = await fetch(`${SERVER_URL}/assignments`,
-                {
-                    method: 'PUT',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(assignment),
-                });
-            if (response.ok) {
-                setMessage("Course Saved");
-                fetchAssignments();
-            } else {
-                const json = await response.json();
-                setMessage('Response error: ' + json.message);
-            }
-        } catch (err) {
-            setMessage('Network error: ' + err);
-        }
-    }
-
-    const addAssignment = async (assignment) => {
-        try {
-            const response = await fetch(`${SERVER_URL}/assignments`,
-                {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(assignment),
-                });
-            if (response.ok) {
-                setMessage("Assignment Added");
-                fetchAssignments();
-            } else {
-                const rc = await response.json();
-                setMessage(rc.message);
-            }
-        } catch (err) {
-            setMessage("Network error: " + err);
-        }
-    }
-
+    const {secNo, courseId, secId} = location.state;
+     
     return(
         <> 
-        <h3> Assignment List </h3>
-        <h4>{message}</h4>
-        <table className='Center'>
-            <thead>
-                <tr>
-                    {headers.map((h, idx) => <th key={idx}>{h}</th>)}
-                </tr>
-            </thead>
-            <tbody>
-                {assignments.map((assignment) => 
-                <tr key={assignment.id}>
-                    <td>{assignment.id}</td>
-                    <td>{assignment.title}</td>
-                    <td>{assignment.dueDate}</td>
-                    <td>{assignment.secNo}</td>
-                    <td><AssignmentGrade assignment={assignment} save={onSave}/></td>
-                    <td><AssignmentUpdate assignment={assignment} save={onSave}/></td>
-                    <td><Button onClick={deleteAlert}>Delete</Button></td>
-                </tr>
-                )}
-            </tbody>
-        </table>
-        <AssignmentAdd section={s} save={addAssignment}/>
+           <h3>Not implemented</h3>
         </>
     );
 }
